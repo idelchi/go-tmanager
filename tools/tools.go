7023366d6d6c0d801a778d2bc86213a30bf12a8f
idelchi <64003206+idelchi@users.noreply.github.com>
2024-12-19 14:32:41 +0100

    dev

--- tools/tools.go
+++ tools/tools.go
@@ -1,5 +1,7 @@
 //go:build tools
 
+// Package tools provides the tools used by the go generate directives in the project.
+// Run `go mod download` to download the tools.
 package tools
 
 import (
